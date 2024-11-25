7dce1fbbb2ca40d15f1a3596a23d13dd53e237e4
sulyma <50264089+sulyma@users.noreply.github.com>
2024-11-25 21:58:15 +1200

    Update module_8_2.py

--- module_8_2.py
+++ module_8_2.py
@@ -4,34 +4,30 @@ def personal_sum(numbers):
 
     for item in numbers:
         try:
-            result += float(item)  # Пробуем привести item к числу и прибавить к результату
-        except (TypeError, ValueError):  # Обрабатываем исключения если item не числовое значение
+            result += item
+        except TypeError:
+            print(f'Некорректный тип данных для подсчёта суммы - {item}')
             incorrect_data += 1
 
     return result, incorrect_data
 
 
 def calculate_average(numbers):
+    if not isinstance(numbers, (list, tuple, set, str)):
+        print('В numbers записан некорректный тип данных')
+        return None
+
+    total_sum, incorrect_data = personal_sum(numbers)
+
     try:
-        # Проверяем, является ли numbers коллекцией
-        if not isinstance(numbers, (list, tuple, set, str)):
-            raise TypeError('В numbers записан некорректный тип данных')
-
-        total_sum, incorrect_count = personal_sum(numbers)
-        valid_count = len(numbers) - incorrect_count  # Количество валидных значений
-
-        # Вычисляем среднее арифметическое
-        average = total_sum / valid_count if valid_count > 0 else 0
-        return average
-
+        average = total_sum / (len(numbers) - incorrect_data)
     except ZeroDivisionError:
         return 0
-    except TypeError as te:
-        print(te)
-        return None
+
+    return average
 
 
 print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
-print(f'Результат 2: {calculate_average([1, "Строка", "3", "Ещё Строка"])}')  # Учитываются только 1 и 3
-# print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
-# print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
